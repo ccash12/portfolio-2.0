e3d559c65bfba5073978acadfc22d5a1b6eb5078
Courtney Cash <84878740+ccash12@users.noreply.github.com>
2023-05-19 12:53:28 -0700

    added about me paragraph

--- myportfolio/src/components/About.js
+++ myportfolio/src/components/About.js
@@ -1,9 +1,22 @@
 
 function About() {
     return(
-        <div>
-        
-        </div>
+        <>
+            <div className="AboutMeTitle">
+                <header>About Me</header>
+            </div>
+            <div className="AboutMeParagraph">
+                <p>Courtney Cash is a fullstack developer with knowledge in Javascript, Ruby, and SQL, with 2 years of experience in web development and software engineering </p>
+                <p>Courtney originally majored in Psychology and Sociology in undergrad but after dippign her toes in the technolohy field she pivoted her career over to software engineering. </p>
+                <p>Courtney then enrolled in a 15 week immersive software engineering bootcamp at The Flatiron School and started her journey at Infosys. 
+                </p>
+                <p>At Infosys, she was able to pick up more skills like trainings in AWS, Oracle, and PL/SQL. </p>
+                <p>
+                    Courtney is excited to continue to grow and learn in the world of technology. 
+                </p>
+                <p>She spends her free time reading, cooking, traveling,hiking, and playing a LOT of ultimate frisbee.</p>
+            </div>
+        </>
     )
 }
 
