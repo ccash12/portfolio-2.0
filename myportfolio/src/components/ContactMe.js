afe149b266f15b0c3bd0cf4954461d720e2ca579
Courtney Cash <84878740+ccash12@users.noreply.github.com>
2023-05-29 13:31:43 -0700

    added emailjs

--- myportfolio/src/components/ContactMe.js
+++ myportfolio/src/components/ContactMe.js
@@ -1,7 +1,57 @@
-function ContactMe() {
+import React from 'react';
+import emailjs from 'emailjs-com';
+
+
+function ContactMe() {  
+    
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_guy505c', 'template_rkkxzrw', e.target, 'user_vlPzjlDuOayKajM3ZxmBk')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+            e.target.reset()
+        };
+
+
     return(
-        <div >
-           
+        <div className="formbox"> 
+            <div className="FormBoxTitle">
+            <h3>Contact Me</h3>
+            </div>
+            <form onSubmit={sendEmail}> 
+            <div className='FormInput'>
+                <input
+                    type="text" 
+                    name='name'
+                    placeholder="Name"
+                />
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                />
+                <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                />
+                <textarea
+                    type="text"
+                    name="message"
+                    placeholder="Your Message">
+                </textarea>
+                <button type='submit'>
+                    Send Message
+                </button>
+                    {/* type='submit'
+                    value="Send Message"
+                /> */}
+                </div>
+            </form>
         </div>
     )
 }
