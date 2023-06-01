66730db479d31da86d820b26d030052b072302e8
Courtney Cash <84878740+ccash12@users.noreply.github.com>
2023-06-01 13:16:43 -0700

    broken image tag, need to look into more

--- myportfolio/src/components/Projects.js
+++ myportfolio/src/components/Projects.js
@@ -1,26 +1,27 @@
 import {Card,Button} from "react-bootstrap"
-import foodshipify from './images/foodshipify.png'
+import acoupleofcooks from "./images/acoupleofcooks.png"
+
 
 function Projects() {
 
     const cardInfo = [
         {image:"", title:"The Ultimate Wedding Organizer", text:"For all things Weddings!!!", button: "The Ultimate Wedding Organizer"},
         {image:"", title:"FoodShipify", text:"This application allows you to browse groceries that you need and have them delivered straight to your door!",button: "Foodshipify"},
-        {image:"", title:"A Couple of Cooks", text:"This is an interactive website that allows you to browse recipes that other users have added and add ones yourself!",button: "A Couple of Cooks"},
+        {image:{acoupleofcooks}, title:"A Couple of Cooks", text:"This is an interactive website that allows you to browse recipes that other users have added and add ones yourself!",button: "A Couple of Cooks"},
         {image:"", title:"Ambient Art Gallery", text:"Art", button: "Ambient Art Gallery"},
     ]
 
     const renderCard = (card,index) => {
         return(
         <Card style={{ width: '18rem' }} key={index} className="box">
-            <Card.Img variant="top" src="holder.js/100px180" src={card.image} />
+            <Card.Img variant="top" src={card.image} />
         <Card.Body>
             <Card.Title >{card.title}</Card.Title>
                 <Card.Text>
                     {card.text}
                 </Card.Text>
-         <Button variant="primary">{card.button}</Button> 
-      </Card.Body>
+            <Button variant="primary">{card.button}</Button> 
+        </Card.Body>
     </Card>
         )
     }
