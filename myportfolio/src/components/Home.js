40686ce6cbb03befaefc271651c794fbf0ecb9a4
Courtney Cash <84878740+ccash12@users.noreply.github.com>
2023-06-14 13:19:50 -0700

    more css

--- myportfolio/src/components/Home.js
+++ myportfolio/src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
             <div className="HomeText">
                 <h1>Hello! I am Courtney Cash</h1>
                 <h4> I am full stack developer and software engineer based in Seattle, Washington</h4>
-                <h6>Javascript, React.js, Node,js Ruby, Ruby on Rails, Java, PL/SQL, MySQL, SQL, Oracle  </h6>
+                <h6>Javascript, React.js, Node,js Ruby, Ruby on Rails, Java, PL/SQL, SQL, Oracle, HTML, CSS  </h6>
             </div>
             <div className="circleCourtney">
                 <img src={C} alt= "C"/>
