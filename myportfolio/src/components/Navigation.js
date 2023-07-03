9b4f566025577cd863b6fb90157379275cad8ee2
Courtney Cash <84878740+ccash12@users.noreply.github.com>
2023-07-03 15:07:22 -0700

    hamburger menu

--- myportfolio/src/components/Navigation.js
+++ myportfolio/src/components/Navigation.js
@@ -10,29 +10,13 @@ function Navigation() {
     <div className="NavBarContainer">
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand to="/"href="home" className="NavBarTitle">Courtney Cash</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+          <Nav className="navBarLinks">
+            <Nav.Link to="/about" href="about">About</Nav.Link>
+            <Nav.Link to="/projects" href="projects">Projects</Nav.Link>
+            <Nav.Link to="/contactme" href="contactme">Contact Me</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
